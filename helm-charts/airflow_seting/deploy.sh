91a707ae4ec5d95a5e6eccfe2184e0f61cf5b60b
unknown <nvietanh1988@gmail.com>
2025-04-09 21:51:09 +0700

    ok

--- helm-charts/airflow_seting/deploy.sh
+++
@@ -1,7 +0,0 @@
-helm repo add apache-airflow https://airflow.apache.org
-kubectl create ns airflow
-kubectl apply -n airflow -f airflow-ssh-git-secret.yaml
-kubectl apply -n airflow -f airflow-worker-crb.yaml
-kubectl create secret generic airflow-db --from-literal=connection=postgresql://airflow:Adgjmptw1@satellite.stackgres.svc.cluster.local:5432/airflow -n airflow
-kubectl create secret generic webserver-secret --from-literal="webserver-secret-key=$(python3 -c 'import secrets; print(secrets.token_hex(16))')" -n airflow
-helm upgrade --install -n airflow --create-namespace airflow apache-airflow/airflow --version 1.8.0 -f values.yaml
